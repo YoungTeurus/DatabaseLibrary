2385bdeae7eab3aba7c7935b71eb9f37fff95035
Little_Teu <s_azarenko@outlook.com>
2021-05-30 17:51:45 +0300

    FIX - Убран instance внутри BasePostgresDataBase.

--- src/main/java/com/github/youngteurus/servletdatabase/database/BasePostgresDataBase.java
+++ src/main/java/com/github/youngteurus/servletdatabase/database/BasePostgresDataBase.java
@@ -3,24 +3,20 @@ package com.github.youngteurus.servletdatabase.database;
 import java.sql.*;
 
 public class BasePostgresDataBase implements DataBase {
-    private static BasePostgresDataBase instance;
     private static BasePostgresDataBase creditServiceInstance;
 
     private final String dbURL;
     private final String user;
     private final String pass;
 
+    private Connection connectionInstance;
 
-    public BasePostgresDataBase(String dbURL, String user, String pass) {
+
+    protected BasePostgresDataBase(String dbURL, String user, String pass) {
         this.dbURL = dbURL;
         this.user = user;
         this.pass = pass;
-        if (instance == null) {
-            instance = this;
-        }
     }
-
-    static Connection connectionInstance;
 
     private void createConnectionInstance() throws DataBaseConnectionException{
         System.out.println("Testing connection to PostgreSQL JDBC");
